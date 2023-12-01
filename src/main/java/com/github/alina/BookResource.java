b293ce7983f24bc856a3ada94514a792509316a9
alinaberlin <alina.ghetler@gmail.com>
2023-12-01 10:26:17 +0100

    Add jersey, mysql dependencies in pom file and create DBC connection and BookResource, add env vars

---
+++ src/main/java/com/github/alina/BookResource.java
@@ -0,0 +1,4 @@
+package com.github.alina;
+
+public class BookResource {
+}
---
+++ src/main/java/com/github/alina/DBCconnection.java
@@ -0,0 +1,11 @@
+package com.github.alina;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBCconnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore", System.getenv("USERNAME"), System.getenv("PASSWORD"));
+    }
+}
