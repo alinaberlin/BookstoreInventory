29435a30cec0fc99346d2fe388b058a9537004e6
alinaberlin <alina.ghetler@gmail.com>
2023-12-01 12:49:22 +0100

    Add method GET req in BookResources

--- src/main/java/com/github/alina/BookResource.java
+++ src/main/java/com/github/alina/BookResource.java
@@ -2,6 +2,7 @@ package com.github.alina;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -15,5 +16,11 @@ public class BookResource {
         return personDAO.getAlBook();
     }
 
+    @GET
+    @Path("/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Book getBook(@PathParam("id") int id){
+        return personDAO.getBook(id);
+    }
 
 }
