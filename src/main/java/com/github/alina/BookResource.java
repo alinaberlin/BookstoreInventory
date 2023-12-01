d8473aef68da5719349020889ee8573d1af116ba
alinaberlin <alina.ghetler@gmail.com>
2023-12-01 12:19:29 +0100

    Initialize a new instance of the BookDAO, add javax.we.rs library and create GET req

--- src/main/java/com/github/alina/BookResource.java
+++ src/main/java/com/github/alina/BookResource.java
@@ -1,4 +1,18 @@
 package com.github.alina;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/books")
 public class BookResource {
+    BookDAO personDAO = new BookDAO();
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Book> getBooks(){
+        return personDAO.getAlBook();
+    }
+
 }
