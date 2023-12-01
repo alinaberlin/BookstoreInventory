a684e9150672525dd2a9526c5b68e48f83b01460
alinaberlin <alina.ghetler@gmail.com>
2023-12-01 10:33:49 +0100

    Add server connection and establish successful JDBC connection

--- src/main/java/com/github/alina/Main.java
+++ src/main/java/com/github/alina/Main.java
@@ -4,14 +4,22 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.net.URI;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        //implement logic for server
         System.out.println("Hello world!");
         String BASE_URI = "http://localhost:8080/";
         ResourceConfig resourceConfig = new ResourceConfig(BookResource.class);
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
+        //add connection for server
         System.out.println("Server started at: " + BASE_URI);
+        try {
+            DBCconnection.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
